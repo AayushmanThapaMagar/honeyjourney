191feb433d16e9b969f20f29d8a413dfa5a1232f
Aayushman Thapa Magar <ayushman2000@outlook.com>
2023-05-06 22:00:32 +0545

    before build

--- components/products/productcard.jsx
+++ components/products/productcard.jsx
@@ -8,25 +8,15 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
-import { StorefrontContext } from "@/provider/storefront-provider";
-import { useContext } from "react";
 
+import Image from "next/image";
 export default function ProductCard({ product }) {
-  const handleAddToCartClick = () => {
-    const { addItem, open, toggleCart, getCart } =
-      useContext(StorefrontContext);
-
-    addItem({ product_id: product.id, quantity: quantity }).then(() => {
-      getCart();
-      toggleCart(!open);
-    });
-  };
 
   return (
     <Card className="w-96 h-120 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-[0_35px_60px_-15px_rgba(135,206,235)]">
       <a href={`/products/${product.slug}`}>
         <CardHeader color="#FCA000" className="relative h-100">
-          <img
+          <Image
             src={product.images[0].file.url}
             alt={product.name}
             className="object-cover object-center"
@@ -41,7 +31,7 @@ export default function ProductCard({ product }) {
       </a>
       <CardFooter divider className="flex items-center justify-between py-3">
         <Typography variant="small">${product.price}</Typography>
-        <Button onClick={() => handleAddToCartClick()}>Add to Cart</Button>
+        {/* <Button onClick={() => handleAddToCartClick()}>Add to Cart</Button> */}
       </CardFooter>
     </Card>
   );
